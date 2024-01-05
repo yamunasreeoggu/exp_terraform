981160a4253eb668044c78a3a1fe52e524d9b023
yamunasreeoggu <yamunasree321@gmail.com>
2024-01-04 23:33:00 -0500

    Practice | Yamuna | added route for pub sub to pub rt and pri sub to pri rt

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -100,12 +100,12 @@ resource "aws_route" "route" {
 
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnets)
-  subnet_id      = var.public_subnets[count.index].id
+  subnet_id      = aws_subnet.private_subnets[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
   count = length(var.private_subnets)
-  subnet_id      = var.private_subnets[count.index].id
+  subnet_id      = aws_subnet.private_subnets[count.index].id
   route_table_id = aws_route_table.private.id
 }
