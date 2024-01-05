d8e89e2719662ee11da474d830ac571ffb16e43b
yamunasreeoggu <yamunasree321@gmail.com>
2024-01-04 22:21:21 -0500

    Practice | Yamuna | added code for subnets

--- main.tf
+++ main.tf
@@ -2,4 +2,7 @@ module "vpc" {
   source = "./modules/vpc"
   env = var.env
   vpc_cidr = var.vpc_cidr
+  public_subnets = var.public_subnets
+  private_subnets = var.private_subnets
+  azs = var.azs
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,28 @@
 resource "aws_vpc" "vpc" {
-  cidr_block       = var.vpc_cidr
+  cidr_block = var.vpc_cidr
   tags = {
     Name = "${var.env}-vpc"
   }
 }
 
+resource "aws_subnet" "public_subnets" {
+  count = length(var.public_subnets) # as we have list of two values for public subnets, we need to declare them using count loop for iteration
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.public_subnets[count.index] # count.index is needed for iteration as i/p has list of two values for public subnets
+  availability_zone = var.azs[count.index]
+
+  tags = {
+    Name = "${var.env}-public_subnets"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count = length(var.private_subnets)
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = {
+    Name = "${var.env}-private_subnets"
+  }
+}
