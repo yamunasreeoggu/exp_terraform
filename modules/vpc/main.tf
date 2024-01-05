1440ad2947639c64ee764d857f36edbf2766e2e0
yamunasreeoggu <yamunasree321@gmail.com>
2024-01-04 22:44:59 -0500

    Practice | Yamuna | added code for ngw

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -41,3 +41,12 @@ resource "aws_eip" "eip" {
     Name = "${var.env}-eip"
   }
 }
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name = "${var.env}-ngw"
+  }
+}
