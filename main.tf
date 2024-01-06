df0b6de8f12c9039bb67d1cf961b6af5c4b499bd
yamunasreeoggu <yamunasree321@gmail.com>
2024-01-05 22:48:59 -0500

    Practice | Yamuna | added private alb code

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "private-lb" {
   source            = "./modules/alb"
   env               = var.env
   alb-type          = "private"
-  alb_sg_allow_cidr = var.vpc_cidr
+  alb_sg_allow_cidr = [var.vpc_cidr]
   internal          = true
   vpc_id            = module.vpc.vpc_id
   subnets           = module.vpc.private_subnets
