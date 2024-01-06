0a5381ff8394fcf88a0e47055b669071c521cec4
yamunasreeoggu <yamunasree321@gmail.com>
2024-01-05 22:18:27 -0500

    Practice | Yamuna | added alb-sg code

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,25 +1,25 @@
-#resource "aws_security_group" "sec-grp" {
-#  name        = "${var.env}-${var.alb-type}-sg"
-#  description = "${var.env}-${var.alb-type}-sg"
-#  vpc_id      = var.vpc_id
-#
-#  ingress {
-#    description      = "HTTP"
-#    from_port        = 80
-#    to_port          = 80
-#    protocol         = "tcp"
-#    cidr_blocks      = var.alb_sg_allow_cidr
-#  }
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#
-#  }
-#
-#  tags = {
-#    Name = "${var.env}-${var.alb-type}-sg"
-#  }
-#}
+resource "aws_security_group" "sec-grp" {
+  name        = "${var.env}-${var.alb-type}-sg"
+  description = "${var.env}-${var.alb-type}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = var.alb_sg_allow_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "${var.env}-${var.alb-type}-sg"
+  }
+}
