841aee44bdcc23b454a12cb463f25334e4368e3a
yamunasreeoggu <yamunasree321@gmail.com>
2024-01-07 22:59:30 -0500

    Practice | Yamuna | added code for asg

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -28,7 +28,7 @@ resource "aws_launch_template" "template" {
   name = "${var.env}-${var.component}"
   image_id = data.aws_ami.ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = aws_security_group.sec-grp.id
+  vpc_security_group_ids = [aws_security_group.sec-grp.id]
 
   tag_specifications {
     resource_type = "instance"
