e075062036b4cdf64944ae5df6702a1e8dd1c47d
yamunasreeoggu <yamunasree321@gmail.com>
2024-01-07 22:30:03 -0500

    Practice | Yamuna | added private alb code

---
+++ modules/app/main.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "sec-grp" {
+  name        = "${var.env}-${var.component}-sg"
+  description = "${var.env}-${var.component}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = var.app-port
+    to_port          = var.app-port
+    protocol         = "tcp"
+    cidr_blocks      = var.vpc_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "${var.env}-${var.component}-sg"
+  }
+}
+
+resource "aws_launch_template" "template" {
+  name = "${var.env}-${var.component}"
+  image_id = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = aws_security_group.sec-grp.id
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "${var.env}-${var.component}"
+    }
+  }
+}
+
---
+++ modules/app/vars.tf
@@ -0,0 +1,6 @@
+variable "env" {}
+variable "component" {}
+variable "vpc_id" {}
+variable "app-port" {}
+variable "vpc_cidr" {}
+variable "instance_type" {}
