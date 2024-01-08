ffc55fa4c335f61c2fcf7d4713b8a2fda3b8f738
yamunasreeoggu <yamunasree321@gmail.com>
2024-01-08 14:48:07 -0500

    Practice | Yamuna | added Iam role and policy to fectch paramerets from store

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -32,11 +32,68 @@ resource "aws_security_group" "sec-grp" {
   }
 }
 
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.component}-role"
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Sid       = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "${var.env}-${var.component}-policy"
+
+    policy = jsonencode({
+      Version   = "2012-10-17",
+      Statement = [
+        {
+          "Sid" : "VisualEditor0",
+          "Effect" : "Allow",
+          "Action" : [
+            "ssm:DescribeParameters",
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource" : "*"
+        }
+      ]
+    })
+  }
+
+  tags = {
+    tag-key = "${var.env}-${var.component}-role"
+  }
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.env}-${var.component}-role"
+  role = aws_iam_role.role.name
+}
+
 resource "aws_launch_template" "template" {
   name = "${var.env}-${var.component}"
   image_id = data.aws_ami.ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.sec-grp.id]
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.instance_profile.name
+  }
+  user_data = base64encode(templatefile("${path.module}/userdata.sh",{
+    role_name = var.component
+    env = var.env
+  }))
 
   tag_specifications {
     resource_type = "instance"
@@ -45,10 +102,6 @@ resource "aws_launch_template" "template" {
       Name = "${var.env}-${var.component}"
     }
   }
-  user_data = base64encode(templatefile("${path.module}/userdata.sh",{
-    role_name = var.component
-    env = var.env
-  }))
 }
 
 resource "aws_autoscaling_group" "asg" {
