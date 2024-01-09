6c1675a1e1a0c9e60b8781fa5a3f1d47c324fc7d
yamunasreeoggu <yamunasree321@gmail.com>
2024-01-08 19:25:56 -0500

    Practice | Yamuna | added trg grp

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -110,6 +110,7 @@ resource "aws_autoscaling_group" "asg" {
   max_size           = var.max_size
   min_size           = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns = [aws_lb_target_group.tg.arn]
 
   launch_template {
     id      = aws_launch_template.template.id
